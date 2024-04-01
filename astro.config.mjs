cf5a84d209e1b2c70b31b9938c21efeb15ed66e6
knnv-ar <106931704+knnv-ar@users.noreply.github.com>
2024-04-01 02:14:41 -0300

    First commit

---
+++ astro.config.mjs
@@ -0,0 +1,38 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'AM1 Lacabanne',
+			customCss: [
+				// Relative path to your custom CSS file
+				'./src/styles/custom.css',
+			],
+			social: {
+				github: 'https://github.com/withastro/starlight',
+			},
+			sidebar: [
+				{
+					label: 'Taller de p5.js',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Clase 1', link: '/taller-p5js/clase-1/' },
+					],
+				},
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', link: '/guides/example/' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
